97b087d930e5be4c8ea6be6223af4ffeb060348e
Oyewole Samuel <oyewolesamuel567@gmail.com>
2024-08-06 10:46:29 +0100

    testing

--- src/assets/components/Mainn.jsx
+++ src/assets/components/Mainn.jsx
@@ -5,7 +5,7 @@ const Mainn = () => {
     <>
     
   <div className="px-4 py-5 my-5 text-center">
-    <img className="d-block mx-auto mb-4" src="http://127.0.0.1:5502/image/connect.png" alt="" width="50%"/>
+    <img className="d-block mx-auto mb-4" src="https://d23yw4k24ca21h.cloudfront.net/assets/home/hero-stream-1408-d9b09849b8d1a4fc47c7078658d6fe6a2092e64c205a72b6b6ee111b3aaf2558.webp" alt="" width="50%"/>
     <h1 className="display-5 fw-bold text-body-emphasis">A Complete Solution</h1>
     <div className="col-lg-6 mx-auto">
       <p className="lead mb-4">We are a comprehensive one-stop-shop for all audio needs - from live streaming and hosting to beautiful, custom players, apps and websites, Our user-friendly tools simplify the production of professional-grade audio, making it accessible for everyone.</p>
